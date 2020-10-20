912d75bbef8985a1ac92429b92b9cbe3252e79e7
Michael <michaelli.sw.eng@gmail.com>
2020-10-19 22:27:58 -0700

    Added solution for 344. Reverse String

---
+++ 344. Reverse String.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/reverse-string/
+
+class Solution {
+    public void reverseString(char[] s) {
+        for(int i = 0; i < s.length/2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+    }
+}
+
+/*
+	Time Complexity: O(n/2) -> O(n)
+	Space Complexity: O(1)
+*/
