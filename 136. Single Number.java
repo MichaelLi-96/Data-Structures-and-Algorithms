fb46d1b1de4c9f0e6a2144fb4b57f881be40902e
Michael <michaelli.sw.eng@gmail.com>
2020-10-22 00:15:50 -0700

    Updated solution for 136. Single Number

--- 136. Single Number.java
+++ 136. Single Number.java
@@ -2,16 +2,16 @@
 
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
         for(int num : nums) {
-            if(!set.contains(num)) {
-                set.add(num);
+            if(!list.contains(num)) {
+                list.add(num);
             }
             else {
-                set.remove(num);
+                list.remove(new Integer(num));
             }
         }
-        return set.iterator().next();
+        return list.get(0);
     }
 }
 
