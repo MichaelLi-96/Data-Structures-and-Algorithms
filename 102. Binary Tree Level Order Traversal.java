a4f96f6ea8feb36a89b60912474489f0022b8fc6
Michael <michaelli.sw.eng@gmail.com>
2020-10-25 21:05:15 -0700

    Added solution for 102. Binary Tree Level Order Traversal

---
+++ 102. Binary Tree Level Order Traversal.java
@@ -0,0 +1,48 @@
+//https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null) {
+            return list;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> row = new ArrayList<>();
+            for(int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                row.add(node.val);
+                if(node.left != null) {
+                    queue.add(node.left);
+                }
+                if(node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            list.add(row);
+        }
+        return list;
+    }
+}
+
+/*
+	Time Complexity: O(n) where n is the number of nodes in the binary tree
+	Space Complexity: O(n) where n is the number of nodes in the binary tree
+*/
