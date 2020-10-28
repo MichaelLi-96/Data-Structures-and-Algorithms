af6e2704eafda4ffe93c221aecd97598ed2f1f60
Michael <michaelli.sw.eng@gmail.com>
2020-10-28 15:47:02 -0700

    Added solution for 155. Min Stack

---
+++ 155. Min Stack.java
@@ -0,0 +1,47 @@
+//https://leetcode.com/problems/min-stack/
+
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        if(x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        int top = stack.pop();
+        if(top == min) {
+            min = stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+/*
+    Time Complexity: O(1) for the push(), pop(), top(), and getMin()
+    Space Complexity: O(n) where n is the maximum size of the stack
+*/
