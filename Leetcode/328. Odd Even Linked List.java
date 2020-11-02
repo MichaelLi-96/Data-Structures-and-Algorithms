b4c37518f3a4f1ec1f07b9aadb5341292b74ec3d
Michael <michaelli.sw.eng@gmail.com>
2020-11-01 23:43:09 -0800

    Added solution for 328. Odd Even Linked List

---
+++ Leetcode/328. Odd Even Linked List.java
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/odd-even-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while(even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
+
+/*
+    Time Complexity: O(n) where n is the number of nodes in the linkedlist
+    Space Complexity: O(1)
+*/
