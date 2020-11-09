2d014b53d7cc4d7bb9f5891a3324e15e7782ff8b
Michael <michaelli.sw.eng@gmail.com>
2020-11-08 22:38:20 -0800

    Added solution for 876. Middle of the Linked List

---
+++ Leetcode/876. Middle of the Linked List.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/middle-of-the-linked-list/submissions/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+
+/*
+	Time Complexity: O(n) where n is the number of nodes in the linkedlist
+	Space Complexity: O(1)
+*/
