a4e3c47965e8e9d46799eebd957a53af0247f651
Michael <michaelli.sw.eng@gmail.com>
2020-11-12 18:46:36 -0800

    Added solution for 700. Search in a Binary Search Tree

---
+++ Leetcode/700. Search in a Binary Search Tree.java
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) {
+            return null;
+        }
+        if(root.val == val) {
+            return root;
+        }
+        TreeNode left = searchBST(root.left, val);
+        TreeNode right = searchBST(root.right, val);
+        if(left != null) {
+            return left;
+        }
+        if(right != null) {
+            return right;
+        }
+        return null;
+    }
+}
+
+/*
+    Time Complexity: O(n) where n is the number of nodes in the tree
+    Space Complexity: O(log(n)) for balanced tree and O(n) for skewed tree where n is the height of the tree 
+*/
