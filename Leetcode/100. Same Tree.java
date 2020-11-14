a33cd638e75f5aec3e96f534df3d8d4b7ea9ad87
Michael <michaelli.sw.eng@gmail.com>
2020-11-13 16:26:28 -0800

    Added solution for 100. Same Tree

---
+++ Leetcode/100. Same Tree.java
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        else if(p == null && q != null || p != null && q == null) {
+            return false;
+        }
+        if(p.val != q.val) {
+            return false;
+        }
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+        return left && right;
+    }
+}
+
+/*
+    Time Complexity: O(n) where n is the number of nonoverlapping nodes in the tree
+    Space Complexity: O(log(n)) for balanced tree and O(n) for skewed tree where n is the height of the tree 
+*/
