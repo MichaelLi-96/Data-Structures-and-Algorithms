e53bd1904729ba2c1e5cc920df7007d5669ec7ce
Michael <michaelli.sw.eng@gmail.com>
2021-01-19 16:04:36 -0800

    Added solution for 189. Rotate Array

--- Leetcode/189. Rotate Array.java
+++ Leetcode/189. Rotate Array.java
@@ -14,8 +14,32 @@ class Solution {
     }
 }
 
-
 /*
     Time Complexity: O(n) where n is the length of the input int array nums
     Space Complexity: O(n) where n is the length of the input int array nums
+*/
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int visited = 0;
+        for(int i = 0; visited < nums.length; i++) {
+            int current = i;
+            int prev = nums[i];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next]; 
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                visited++;
+                
+            } while(current != i);
+        }
+    }
+}
+
+/*
+    Time Complexity: O(n) where n is the length of the input int array nums
+    Space Complexity: O(1)
 */
