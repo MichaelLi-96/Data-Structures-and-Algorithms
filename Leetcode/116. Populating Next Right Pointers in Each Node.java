82e1d54aea237c937e2b2ee5de47ce1851682ada
Michael <michaelli.sw.eng@gmail.com>
2021-01-31 20:49:04 -0800

    Added solution for 116. Populating Next Right Pointers in Each Node

---
+++ Leetcode/116. Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,100 @@
+//https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                Node nd = queue.poll();
+                if(i < size - 1) {
+                    nd.next = queue.peek();
+                }
+                if(nd.left != null) {
+                    queue.add(nd.left);
+                }
+                if(nd.right != null) {
+                    queue.add(nd.right);
+                }
+            }
+        }
+        return root;
+    }
+}
+
+/*
+    Time Complexity: O(n) where n is the number of nodes in the tree
+    Space Complexity: O(n) where n is the number of nodes in the tree
+*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) {
+            return null;
+        }
+        if(root.left != null) {
+            root.left.next = root.right;
+        }
+        if(root.right != null && root.next != null) {
+            root.right.next = root.next.left;
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
+}
+
+/*
+    Time Complexity: O(n) where n is the number of nodes in the tree
+    Space Complexity: O(log(n)) for balanced tree and O(n) for skewed tree where n is the height of the tree 
+*/
