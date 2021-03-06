530cd3e4a000c31a9fa381dee20607b96d062c87
Michael <michaelli.sw.eng@gmail.com>
2021-03-05 23:28:23 -0800

    Added solution for 144. Binary Tree Preorder Traversal

---
+++ Leetcode/144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,83 @@
+//https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// Recursively
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preorder(root, list);
+        return list;
+    }
+    
+    public void preorder(TreeNode root, List<Integer> list) {
+        if(root == null) {
+            return;
+        }
+        list.add(root.val);
+        preorder(root.left, list);
+        preorder(root.right, list);
+    }
+}
+
+/*
+    Time Complexity: O(n) where n is the number of nodes in the tree
+    Space Complexity: O(log(n)) for balanced tree and O(n) for skewed tree where n is the height of the tree 
+*/
+    
+// Iteratively 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if(node.right != null) {
+                stack.push(node.right);
+            }
+            if(node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+}
+
+/*
+    Time Complexity: O(n) where n is the number of nodes in the tree
+    Space Complexity: O(n) where n is the number of nodes in the tree
+*/
